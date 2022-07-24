78b7b3b4dccecd49249fd48700210ca90886acff
ManvelmManucharyan <manvelmanucharyan98@gmail.com>
2022-07-24 14:58:23 +0400

    Image upload

--- routes/films.js
+++ routes/films.js
@@ -1,18 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
 const Films = require('../models/films');
 const Directors = require('../models/directors');
-const uploadPath = path.join('public', Films.coverImageBasePath);
 const imageMimeTypes = ['image/jpeg','image/jpg', 'image/png', 'image/gif'];
-const upload = multer({
-    dest: uploadPath,
-    fileFilter: (req, file, callback)=>{
-        callback(null, imageMimeTypes.includes(file.mimetype));
-    }
-});
 
 router.get('/', async (req, res)=>{
     let query = Films.find({});
@@ -40,32 +30,22 @@ router.get('/new', async (req, res)=>{
     renderNewPage(res, new Films());
 });
 
-router.post('/', upload.single('cover'), async (req, res)=>{
-    const fileName = req.file != null ? req.file.filename : null;
+router.post('/', async (req, res)=>{
     const films = new Films({
         title: req.body.title,
         director: req.body.director.trim(),
         publishDate: new Date(req.body.publishDate),
         duration: req.body.duration,
-        coverImageName: fileName,
         description: req.body.description
     });
+    saveCover(films, req.body.cover);
     try {
         const newFilm = await films.save();
         res.redirect(`films`);
     } catch {
-        if(films.coverImageName != null){
-            removeCoverImageName(films.coverImageName);
-        }
         renderNewPage(res, films, true);
     }
 });
-
-function removeCoverImageName(fileName){
-    fs.unlink(path.join(uploadPath, fileName), err => {
-       if(err) console.error(err);
-    });
-}
 
 async function renderNewPage (res, films, hasError = false){
     try{
@@ -78,6 +58,15 @@ async function renderNewPage (res, films, hasError = false){
         res.render('films/new', params);
     } catch{
         res.redirect('/films');
+    }
+}
+
+function saveCover(films, coverEncoded){
+    if(coverEncoded == null) return;
+    const cover  = JSON.parse(coverEncoded);
+    if(cover != null && imageMimeTypes.includes(cover.type)){
+        films.coverImage = new Buffer.from(cover.data, 'base64');
+        films.coverImageType = cover.type;
     }
 }
 
